a98d4a28747fc921f37e1256c14c90c5043e5cbb
Emil <emiles1997@gmail.com>
2022-09-21 18:13:33 +0300

    дз иправил

--- src/AutoTransport.java
+++
@@ -1,9 +0,0 @@
-public class AutoTransport extends Transport {
-    public AutoTransport(String modelName, int wheelsCount) {
-        super(modelName, wheelsCount);
-    }
-
-    public void diagnostics() {
-
-    }
-}
